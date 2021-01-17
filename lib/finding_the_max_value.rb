ff022b1481b4f491fb1288246d38bb57097513f4
patrick simmons <hadroncollides@gmail.com>
2021-01-17 19:09:18 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,10 +1,11 @@
 def find_max_value(array)
-  count = 0 
+  count = 0
   max_value = -1
   while count < array.length do
     if max_value < array[count]
       max_value = array[count]
-    count += 1 
+    end
+    count += 1
   end
   max_value
 end
